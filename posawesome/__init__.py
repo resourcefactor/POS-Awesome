18dc166b17c873ec19a56971fe43fdcc301b9a76
hamzaali15 <malikhamzaali48@gmail.com>
2025-08-23 13:22:47 +0500

    changes pushed

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-# import frappe
+import frappe
 
 __version__ = "6.3.0"
 
 
-# def console(*data):
-#     frappe.publish_realtime("toconsole", data, user=frappe.session.user)
+def console(*data):
+    frappe.publish_realtime("toconsole", data, user=frappe.session.user)
